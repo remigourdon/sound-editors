c5d4cd1d1b7e6422951b146053ef45d7532941cd
remigourdon <contact@remigourdon.io>
2014-05-07 11:25:30 +0200

    Creates the Sound abstract class.

---
+++ framework/Sound.java
@@ -0,0 +1,16 @@
+package framework;
+
+/**
+ * Represents a sound entity.
+ *
+ * A sound is represented basically by a frequency and a duration.
+ * It can then be modified by adding different modifiers in a
+ * specified order.
+ * It is the core of the model in our MVC design pattern implementation.
+ */
+public abstract class Sound {
+
+    public Sound() {
+
+    }
+}
