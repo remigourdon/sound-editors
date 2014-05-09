decd574956ef6400881a327581f8a324f294908b
tchax <moriceau.hichame@gmail.com>
2014-05-09 16:42:20 +0200

    Coding the update method.v

--- framework/TemporalView.java
+++ framework/TemporalView.java
@@ -16,12 +16,30 @@ public class TemporalView extends JFrame implements View {
      * @param Observable o   The sound model
      * @param Object     obj The data array (corresponding to the sound)
      */
-    public void update(Observable o, Object obj) {
-	// Erase the curve
+    public void update(Observable o, Object data) {
+    	
+    	Sound s = o;
+    	
+    	// Erase the curve
+    	
+    	/* plot the data byte array */
+    	// abcisses = index
+    	// ordonnées = amp
+	
 
-	// Build the new one
+	   int height = getHeight();
+	   int middle = height/2;
 
-	// display the changes
+	   // run through the whole array
+	   for (int i=0; i<data.length; i++)
+	   {
+	      int sample = data[i];
+	      int amplitude = sample*middle;
+	      g.drawLine(i, middle+amplitide, i, middle-amplitude); 
+	   }  
+    	
+
+    	// display the changes
     }
 
     /*-----------------*/
@@ -32,7 +50,7 @@ public class TemporalView extends JFrame implements View {
      * Build a TemporalView.
      * @param Sound s The sound to be displayed
      */
-    public TemporalView(Sound s){
+    public TemporalView(){
 	
 	// Build the frame
 	JFrame frame = new JFrame();
