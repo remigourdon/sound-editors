717b9d82282881180edee285257f00d48da53ee6
remigourdon <contact@remigourdon.io>
2014-05-09 16:29:47 +0200

    Creates Generator interface.

---
+++ framework/generators/Generator.java
@@ -0,0 +1,15 @@
+package framework.generators;
+
+/**
+ * Interface to represent a wave generator.
+ */
+public interface Generator {
+    /**
+     * Generates the signal with the given frequency and duration.
+     * @param  f  the frequency of the sample in hertzs
+     * @param  d  the duration of the sample in milliseconds
+     * @param  sr the sample rate
+     * @return    the generated sample as an array of bytes
+     */
+    public byte[] generate(int f, double d, int sr);
+}
