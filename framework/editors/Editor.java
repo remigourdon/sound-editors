85419e41fb61ecb8245953df5f2dbcd65d04dd4f
remigourdon <contact@remigourdon.io>
2014-05-12 17:13:03 +0200

    The variable player in Editor is now protected.

--- framework/editors/Editor.java
+++ framework/editors/Editor.java
@@ -59,5 +59,5 @@ public abstract class Editor extends JFrame implements Observer {
      */
     public abstract JComponent createCentralComponent();
 
-    private Player player;
+    protected Player player;
 }
