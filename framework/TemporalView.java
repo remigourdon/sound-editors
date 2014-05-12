353115540360469170e2baa7f96380bd8880468f
tchax <moriceau.hichame@gmail.com>
2014-05-12 11:32:50 +0200

    Moving view files to subdirectory.

--- framework/TemporalView.java
+++ framework/TemporalView.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+
 
 
 
@@ -16,35 +18,14 @@ public class TemporalView extends JFrame implements View {
      * @param Observable o   The sound model
      * @param Object     obj The data array (corresponding to the sound)
      */
-    public void update(Observable o, Object data) {
+    public void update(Observable o, Object theData) {
     	
-    	Sound s = o;
-    	
-    	// Erase the curve
-    	
-    	/* plot the data byte array */
-    	// abcisses = index
-    	// ordonnées = amp
-	
-
-	   int height = getHeight();
-	   int middle = height/2;
-
-	   // run through the whole array
-	   for (int i=0; i<data.length; i++)
-	   {
-	      int sample = data[i];
-	      int amplitude = sample*middle;
-	      g.drawLine(i, middle+amplitide, i, middle-amplitude); 
-	   }  
-    	
-
-    	// display the changes
+		// Retreiving the sound model
+		Sound s = o;
+		// Retreiving the sound buffer
+		data = theData;
+		
     }
-
-    /*-----------------*/
-    /*BUILDING THE VIEW*/
-    /*-----------------*/
 
     /**
      * Build a TemporalView.
@@ -52,23 +33,20 @@ public class TemporalView extends JFrame implements View {
      */
     public TemporalView(){
 	
-	// Build the frame
-	JFrame frame = new JFrame();
-	frame.setBounds(200,200, 300, 130);
-	System.out.println(System.getProperty("user.dir")); // Where .mp3 should be located
-        
-
-	// display the WAVEFORM ------------
-	
-            
-
-	// ---------------------------------
-
-	// display the whole frame
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	pack();
-	setVisible(true);
+	// Settings
+    setTitle("Signal plotting");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(400, 200);
+    
+    signal = new SignalPanel();
+    getContentPane().add(signal);
+    
+    setVisible(true);
 	
     }
+    
+    
+    // the waveforw display
+    SignalPanel signal;
 
 }
