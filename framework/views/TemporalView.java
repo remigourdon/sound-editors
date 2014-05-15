9707943c6e38f78f8a96d4b01ed09f29b520ce75
tchax <moriceau.hichame@gmail.com>
2014-05-16 01:02:37 +0200

    TemporalView is now a JPanel.

--- framework/views/TemporalView.java
+++ framework/views/TemporalView.java
@@ -1,8 +1,12 @@
-package framework.views;
-
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
 import java.util.Observable;
+
 import javax.swing.*;
-import java.util.Observer;
 
 
 /**
@@ -10,51 +14,122 @@ import java.util.Observer;
  *	It uses the SignalPanel class to draw a temporal representation of the model's buffer.
  *
  */
-public class TemporalView extends JFrame implements View {
-
-    /**
+public class TemporalView extends JPanel implements View {
+    
+	
+	/**
      * Implementation of view's method.
-     *
-     *
+     * 
+     * 
      * The update method is called when a change occurs in the model (theSound).
      * @param Observable o   			The sound model.
-     * @param Object     dataChanged 	Is a boolean indicating if the buffer has been modified.
+     * @param Object     dataChanged 	Is a boolean indicating if the model has been modified.
      * @Override
      */
     public void update(Observable o, Object dataChanged) {
-
+    	
 		// Retrieving the sound model
 		Sound s = (Sound) o;
 		// Retrieving the sound buffer
 		Double[] data = s.getData();
-
-
-		if( (boolean) dataChanged) { // Might not be required
-			signalPanel.drawData(data);
+		
+		
+		if( (boolean) dataChanged) {
+			this.drawData(data);
 		}
-
     }
 
     /**
      * Build a TemporalView.
+     * @param int h	Height of the panel.
+     * @param int w Width of the panel.
      * @param Sound s The sound to be displayed.
      */
-    public TemporalView(){
+    public TemporalView(int h, int w){
 
-	// Settings
-    setTitle("Signal plotting");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(400, 200);
-
-    signalPanel = new SignalPanel();
-    getContentPane().add(signalPanel);
-
-    setVisible(true);
-
+    	height = h;
+    	width = w;
+    	
+    }
+    
+    /**
+     * Main method of this class.
+     * Draws the waveform.
+     * @param	Double	buffer	the data array to be plotted
+     */
+    public void drawData(Double[] buffer) {
+    	
+    	// Data display settings
+    	g2.setColor(Color.WHITE);
+    	g2.clearRect( 0, 0, width, height);
+    	
+    	// run through the buffer
+    	for(int i = 0 ; i < buffer.length ; i++) {
+    		double prevPoint = (double) buffer[i];
+    		// MEMENTO : Line2D.Double( x1, y1, x2, y2)
+    		// Previous point = (x1,y1) & Current point = (x2,y2)
+    		g2.draw( new Line2D.Double(
+    				buffer.length - i ,
+    				currentPoint + height/2 ,
+    				buffer.length - (i+1),
+    				prevPoint + height/2
+    				));
+    		
+    		System.out.println("Buffer = "+ buffer[i]);
+    		currentPoint = prevPoint;
+    		
+    	}
+    	repaint();
     }
 
-
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+    	// calling mother's method
+    	super.paintComponent(g);
+    	
+		if(bufferedImage == null){
+    		init();
+    	}
+		// ( Image, x, y, theOberserver )
+    	g.drawImage(bufferedImage, 0, 0, this);
+    }
+    
+    /**
+     * Defines the fundamentals aspects settings
+     * Helper method
+     * 
+     * 
+     */
+    private void init(){
+    	// size settings
+    	width = getWidth();
+    	height = getHeight();
+    	
+    	// image settings :
+    	// 8 bit
+    	// Alpha Red Green Blue
+    	bufferedImage = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB);
+    	
+    	g2 = bufferedImage.createGraphics();
+    	g2.setBackground(Color.black);
+    	
+    	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+    }
+    
     // the waveform display
-    private SignalPanel signalPanel;
+    //private SignalPanel signalPanel;
 
+    // Panel visual settings
+    private int height;
+    private int width;
+    
+    // Used to draw the sound
+    private BufferedImage bufferedImage;
+    public Graphics2D g2;
+    
+    // Memorizing the y1 point (c.f drawData() )
+    private double currentPoint;
+    
 }
