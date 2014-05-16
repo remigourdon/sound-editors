7ade0b0a4591bfdb2e0ff4c92e311f9671ad7733
tchax <moriceau.hichame@gmail.com>
2014-05-16 18:25:17 +0200

    Working on the FFT view. Need to scale the display.

--- framework/views/FFTView.java
+++ framework/views/FFTView.java
@@ -14,14 +14,13 @@ import javax.swing.*;
  *	It uses the SignalPanel class to draw a temporal representation of the model's buffer.
  *
  */
-public class TemporalView extends JPanel implements View {
+public class FFTView extends JPanel implements View {
     
 	
 	/**
      * Implementation of view's method.
      * 
-     * 
-     * The update method is called when a change occurs in the model (theSound).
+     * The update method is called when a change occurs in the model (Sound).
      * @param Observable o   			The sound model.
      * @param Object     dataChanged 	Is a boolean indicating if the model has been modified.
      * @Override
@@ -45,7 +44,7 @@ public class TemporalView extends JPanel implements View {
      * @param int w Width of the panel.
      * @param Sound s The sound to be displayed.
      */
-    public TemporalView(int h, int w){
+    public FFTView(int h, int w){
 
     	height = h;
     	width = w;
@@ -55,125 +54,61 @@ public class TemporalView extends JPanel implements View {
     /**
      * Main method of this class.
      * Draws the waveform.
-     * @param	Double	buffer	the data array to be plotted
+     * @param	Double	data	the data array of the signal
      */
-    public void drawData(Double[] data/*buffer*/) {
-/* This is how to convert Double[] to double[]
- * 		//Converts an array of object Characters to primitives.	
-    	public static char[] toPrimitive(final Character[] array) {
-        if (array == null) {
-            return null;
-        } else if (array.length == 0) {
-            return EMPTY_CHAR_ARRAY;
-        }
-        final char[] result = new char[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = array[i].charValue();
-        }
-        return result;
-    }
-  */
-    	/*
-    	double[] buffer = new double[data.length];
-        double[] in = new double[data.length];
-        
-        */
-    	int fft_size = 4096;
-        int slope = fft_size / 2; //how much do we move the window
-		int frames = (int)(2 / slope) - 1;//getting number of frames, discard last part if minor than size. Not ideal
-    	
-/*
+    public void drawData(Double[] data) {
 
-    	FFT fft = new FFT(fft_size);
-        fft.makeWindow();
-    	double[] buffer = new double[fft_size];
-    	double[] in = new double[fft_size];
-    	
-    	
-    	// fill it
-    	for (int i = 0; i < data.length; i++) {
-            in[i] = data[i].longValue();
-            //System.out.println(""+data[i]);
-        }
-        
-		fft.fft( in, buffer);
-*/		
+    	/* -- Convert Double[] to double[] --  */
+        // LET'S MAKE IT A FLOAT[] !
 	
-
-		//FFT
-		FFT fft = new FFT(fft_size);
-		double[] window = fft.getWindow();
-		double[] re = new double[fft_size]; //array for the real part
-		double[] im = new double[fft_size];	//array for the imaginary part. NOT USED FOR NOW
+    	double[] res= new double[data.length];
+    	for (int i = 0; i < data.length; i++) {
+            res[i] = data[i].doubleValue();
+        }
 		
-		float[] centroids = new float[frames]; //array for the centroids of the entire file
-		float[] energies = new float[frames]; //array for the energies of the entire file
-		
-		for(int f = 0; f < frames; f++){
-			//copy slide of signal to re normalized to -1 to 1
-			//fill im with zeros
-			//multiply signal for window
-			for(int i = 0; i < fft_size; i++){
-				re[i] = music[(f * slope) + i] * 1.0 / (Short.MAX_VALUE + 1);
-				re[i] *= window[i];
-				im[i] = 0;
-			}
-			fft.fft(re, im); //call of the magic function! :-) Thanks to the MeapSoft guys!
-
-			//copy fft data to the array and send to other thread. Remember, Only half of the array is necessary
-			ArrayList<Float> fftData = new ArrayList<Float>();
-			fftData.add(new Float(2)); //heather TWO for fft
-			fftData.add(new Float(frames)); //keep track of how many frames
-			fftData.add(new Float(fft_size / 2)); //size of fft_window
-			fftData.add(new Float(f)); //counter of current frame
-			
-			//these two values are used for getting the Spectral Centroid
-			//http://en.wikipedia.org/wiki/Spectral_centroid
-			double centroidFnXn = 0;
-			double centroidXn = 0;
-			
-			double energy = 0;
-			
-			//after heuristic observation it seems the max that the fft can give is around 70
-			//this for loop is where we traverse all the bands of the FFT -it can be expensive
-			for(int i = 0; i < fft_size / 2; i++){
-				fftData.add((float)Math.sqrt(Math.abs(re[i]))); //remove negatives
-				
-				//for the centroid we keep value of the 
-				centroidFnXn += i * Math.abs(re[i]); //keeping track of the magnitude of each bin multipied by it's index, it does the trick!
-				centroidXn += Math.abs(re[i]); //keep track of the total value of the addition of the magnitudes of all the bins
-				
-				//For the energy we just add all the values of all the bands
-				energy += Math.sqrt(Math.abs(re[i])); //use of the Math.sqrt as a way to compress the values into a smaller range
-			}
-			
-			centroids[f] = (float)(centroidFnXn / centroidXn);
-
-			energies[f] = (float)(energy);
-			
-			publishProgress(fftData);
-		}
-		
+    	
+        /* -- APPLY THE ALGORITHM --  */
+        
+    	Fft fft = new Fft();
+    	fft.transform(res, res);
+    	double[] output = new double[data.length];
+    	//output =  res;
+    	
+    	for (int i = 0; i < data.length; i++) {
+            output[i] = - res[i];
+            // take off the imaginary
+            if(output[i] > 0)
+            	output[i] = 0;
+        }
+    	
+    	/* -- DISPLAY --  */
+    	
     	// Data display settings
     	g2.setColor(Color.WHITE);
     	g2.clearRect( 0, 0, width, height);
-    	
+    	g.setColor(Color.GREEN);
+    	g.clearRect(0, 0, width, height);
     	// run through the buffer
-    	for(int i = 0 ; i < buffer.length ; i++) {
-    		double prevPoint = (double) buffer[i];
+    	for(int i = 0 ; i < output.length ; i++) {
+    		double prevPoint = (double) output[i];
     		// MEMENTO : Line2D.Double( x1, y1, x2, y2)
     		// Previous point = (x1,y1) & Current point = (x2,y2)
     		g2.draw( new Line2D.Double(
-    				buffer.length - i ,
+    				output.length - i ,
     				currentPoint + height/2 ,
-    				buffer.length - (i+1),
+    				output.length - (i+1),
     				prevPoint + height/2
     				));
-    		
-    		//System.out.println("Buffer = "+ buffer[i]);
     		currentPoint = prevPoint;
-    		
-    	}
+    	};
+
+    	// adding the x axis
+    	g.draw( new Line2D.Double(
+    			0 ,
+				height/2,
+				width,
+				height/2
+				));
     	repaint();
     }
 
@@ -207,6 +142,8 @@ public class TemporalView extends JPanel implements View {
     	bufferedImage = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB);
     	
     	g2 = bufferedImage.createGraphics();
+    	g = bufferedImage.createGraphics();
+    	
     	g2.setBackground(Color.black);
     	
     	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
@@ -223,6 +160,8 @@ public class TemporalView extends JPanel implements View {
     // Used to draw the sound
     private BufferedImage bufferedImage;
     public Graphics2D g2;
+    // Used to draw the x axis
+    public Graphics2D g;
     
     // Memorizing the y1 point (c.f drawData() )
     private double currentPoint;
