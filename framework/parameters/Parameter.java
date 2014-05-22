9564c644b6386be2ac47289e21dc6bc915f0b94c
remigourdon <contact@remigourdon.io>
2014-05-22 19:10:25 +0200

    Parameter is an abstract class.

--- framework/parameters/Parameter.java
+++ framework/parameters/Parameter.java
@@ -3,27 +3,43 @@ package framework.parameters;
 import java.util.Observable;
 
 /**
- * Generic class to represent any kind of parameter.
+ * Abstract class to represent any kind of parameter.
  */
-public class Parameter<V> extends Observable {
-    public Parameter<V>(String n) {
+public class Parameter extends Observable {
+    /**
+     * Create a Parameter object.
+     * @param  n the name of the parameter
+     */
+    public Parameter(String n) {
         name = n;
+    }
+
+    /**
+     * Attach a new ParameterEditor to the Parameter object.
+     * @return the ParameterEditor newly attached
+     */
+    public ParameterEditor attachEditor() {
+        ParameterEditor editor = new ParameterEditor(this);
+        addObserver(editor);
+        return editor;
     }
 
     /**
      * Get the current value of the Parameter object.
      * @return the value
      */
-    public V getValue() {
+    public Object getValue() {
         return value;
     }
 
     /**
-     * Set a new value in the Parameter  object.
+     * Set a new value in the Parameter object.
      * @param v the new value
      */
-    public void setValue(V v) {
+    public void setValue(Object v) {
         value = v;
+        setChanged();
+        notifyObservers(v);
     }
 
     public String toString() {
@@ -31,5 +47,5 @@ public class Parameter<V> extends Observable {
     }
 
     private String  name;
-    private V       value;
+    private Object  value;
 }
