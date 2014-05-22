d3eda98c3415248c0b5d89dc042de54d61f873cb
remigourdon <contact@remigourdon.io>
2014-05-22 18:36:15 +0200

    Parameter is now a class of generic type.

--- framework/parameters/Parameter.java
+++ framework/parameters/Parameter.java
@@ -1,18 +1,35 @@
+package framework.parameters;
+
+import java.util.Observable;
+
 /**
- * Interface to represent any kind of parameter.
+ * Generic class to represent any kind of parameter.
  */
-public interface Parameter {
+public class Parameter<V> extends Observable {
+    public Parameter<V>(String n) {
+        name = n;
+    }
+
     /**
      * Get the current value of the Parameter object.
      * @return the value
      */
-    public Object getValue();
+    public V getValue() {
+        return value;
+    }
 
     /**
      * Set a new value in the Parameter  object.
      * @param v the new value
      */
-    public void setValue(Object v);
+    public void setValue(V v) {
+        value = v;
+    }
 
-    public String toString();
+    public String toString() {
+        return name;
+    }
+
+    private String  name;
+    private V       value;
 }
