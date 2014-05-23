a59b6d9341abcaf9b0bca188d4fdaad1939a2dea
remigourdon <contact@remigourdon.io>
2014-05-23 17:25:57 +0200

    ParameterEditor is no longer an Observer of Sound.
    All the graphical updates are done directly into the Parameter objects.

--- framework/editors/SoundEditor.java
+++ framework/editors/SoundEditor.java
@@ -3,8 +3,6 @@ package framework.editors;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import java.util.Observable;
 import java.util.Observer;
@@ -12,19 +10,20 @@ import java.util.Observer;
 import framework.Sound;
 import framework.generators.Generator;
 import framework.generators.GeneratorPrototyper;
+import framework.parameters.Parameter;
 
 /**
  * Editor for Sound object.
  *
  * Allow the user to modify the frequency, duration and amplitude.
  */
-public class SoundEditor extends JPanel implements Observer {
+public class SoundEditor extends JPanel {
     /**
      * Create a SoundEditor object.
      * @param  s the associated Sound
      */
     public SoundEditor(Sound s) {
-        sound = s;
+        sound   = s;
 
         // Generator selector
         final JComboBox generatorsList = new JComboBox(GeneratorPrototyper.getPrototypes());
@@ -36,47 +35,11 @@ public class SoundEditor extends JPanel implements Observer {
         });
         add(generatorsList);
 
-        // Frequency input
-        add(new JLabel("Frequency:"));
-        frequencyField  = new JTextField(sound.getFrequency().toString());
-        frequencyField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sound.setFrequency(Double.parseDouble(frequencyField.getText()));
-            }
-        });
-        add(frequencyField);
-
-        // Duration input
-        add(new JLabel("Duration:"));
-        durationField   = new JTextField(sound.getDuration().toString());
-        durationField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sound.setDuration(Double.parseDouble(durationField.getText()));
-            }
-        });
-        add(durationField);
-
-        // Amplitude input
-        add(new JLabel("Amplitude:"));
-        amplitudeField  = new JTextField(sound.getAmplitude().toString());
-        amplitudeField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sound.setAmplitude(Double.parseDouble(amplitudeField.getText()));
-            }
-        });
-        add(amplitudeField);
-    }
-
-    public void update(Observable o, Object arg) {
-        if(o == sound) {
-            frequencyField.setText(sound.getFrequency().toString());
-            durationField.setText(sound.getDuration().toString());
-            amplitudeField.setText(sound.getAmplitude().toString());
+        // Generic parameters
+        for(Parameter p : s.getParameters()) {
+            add(p.attachEditor());
         }
     }
 
-    private Sound sound;
-    private JTextField frequencyField;
-    private JTextField durationField;
-    private JTextField amplitudeField;
+    private Sound                       sound;
 }
