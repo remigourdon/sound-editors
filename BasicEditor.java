841572ea42fc9b8ab20c8e52f579722a4701816d
remigourdon <contact@remigourdon.io>
2014-05-24 17:45:23 +0200

    BasicEditor now only displays label and settings button for each Sound.

--- BasicEditor.java
+++ BasicEditor.java
@@ -1,11 +1,16 @@
 import javax.swing.JComponent;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import framework.editors.Editor;
 import framework.Sound;
 import framework.Player;
-import framework.views.TemporalView;
+//import framework.views.TemporalView;
 
 /**
  * Basic implementation of the framework.
@@ -21,13 +26,30 @@ public class BasicEditor extends Editor {
             centralComponent,
             BoxLayout.Y_AXIS));
 
+        int i = 0;
         for(Sound s : player.getAllSounds()) {
+            final Sound sound = s;
+
             JPanel linePanel = new JPanel();
-            TemporalView v = new TemporalView(200, 100);
-            s.attachView(v);
-            linePanel.add(v);
-            linePanel.add(s.attachEditor());
+
+            JLabel label = new JLabel("Sound " + i);
+            linePanel.add(label);
+
+            //TemporalView v = new TemporalView(200, 100);
+            //s.attachView(v);
+            //linePanel.add(v);
+
+            JButton paramButton = new JButton(new ImageIcon("framework/editors/icons/settings.png"));
+            paramButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    sound.attachEditor();
+                }
+            });
+            linePanel.add(paramButton);
+
             centralComponent.add(linePanel);
+
+            i++;
         }
 
         return centralComponent;
