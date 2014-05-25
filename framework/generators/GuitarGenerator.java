3b720673b56215fae68aab8487f04340d07f34de
remigourdon <contact@remigourdon.io>
2014-05-25 19:12:36 +0200

    Everything is now done in the GuitarGenerator.

--- framework/generators/GuitarGenerator.java
+++ framework/generators/GuitarGenerator.java
@@ -3,23 +3,31 @@ package framework.generators;
 import java.util.Arrays;
 
 import framework.util.RingBuffer;
-import framework.util.GuitarString;
 
 /**
  * A guitar string Generator based on the Karplus-Strong algorithm.
  */
 public class GuitarGenerator extends Generator {
     public Double[] generate(Double f, Double d, Double a) {
+        // Prepare the result array
         int         N       = (int)(Generator.SAMPLE_RATE * d);
         Double[]    result  = new Double[N];
+        Arrays.fill(result, 0.);
 
-        GuitarString string = new GuitarString(f);
+        // Prepare the string array
+        int Ns = (int)(Generator.SAMPLE_RATE / f);
+        RingBuffer buffer = new RingBuffer(Ns);
 
-        string.pluck();
+        // Pluck the string
+        // It is done by filling with random values between -0.5 and 0.5 (noise)
+        for(int i = 0; i < Ns; i++)
+            buffer.enqueue(Math.random() - 0.5);
 
+        // Now we apply the Karplus-Strong algorithm
+        // Product of the average of the two first samples and the energy decay factor
         for(int i = 0; i < N; i++) {
-            result[i] = string.sample();
-            string.tic();
+            buffer.enqueue((buffer.dequeue() + buffer.peek()) / 2 * ENERGY_DECAY_FACTOR);
+            result[i] = buffer.peek();
         }
 
         return result;
@@ -28,4 +36,6 @@ public class GuitarGenerator extends Generator {
     public String toString() {
         return "Guitar";
     }
+
+    Double ENERGY_DECAY_FACTOR = 0.996;
 }
