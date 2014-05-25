8be149f6b9a4569d3c18aca94d7a0d2d87be4f68
remigourdon <contact@remigourdon.io>
2014-05-26 00:12:10 +0200

    Test both editors.

--- TestEditor.java
+++ TestEditor.java
@@ -5,7 +5,7 @@ public class TestEditor {
     public static void main(String[] args) {
         GuitarGenerator g = new GuitarGenerator();
 
-        Player p = new Player();
+        Player pl1 = new Player();
 
         if(args.length % 3 == 0) {
             for(int i = 0; i < args.length / 3; i++) {
@@ -15,10 +15,13 @@ public class TestEditor {
                     Double.parseDouble(args[3 * i + 1]),
                     Double.parseDouble(args[3 * i + 2]));
 
-                p.addSound(sound);
+                pl1.addSound(sound);
             }
         }
 
-        BasicEditor editor = new BasicEditor(p);
+        BasicEditor be = new BasicEditor(pl1);
+
+        Player       pl2 = new Player();
+        MatrixEditor me = new MatrixEditor(pl2);
     }
 }
